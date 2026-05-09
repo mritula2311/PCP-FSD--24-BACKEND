1fa891bc507e14b59f4863ee6511eeb687607879
mritula2311 <mritula2311@gmail.com>
2026-05-09 11:05:44 +0530

    Configure DNS resolvers in server startup

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv';
+import dns from 'node:dns';
 dotenv.config();
 
 import connectDB from './src/config/db.js';
 import app from './src/app.js';
+
+dns.setDefaultResultOrder('ipv4first');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const PORT = process.env.PORT || 5000;
 
